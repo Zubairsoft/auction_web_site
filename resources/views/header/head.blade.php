a4c44f4b75bc3c931fcb7545f8d0fd10ad6f2aaf
samia saeed <samsaeed738@gmail.com>
2022-05-20 21:09:06 +0300

    final changes styles of all pages

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -8,11 +8,12 @@
 
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('assets/css/icons.min.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+  <link href="{{asset('assets/css/ionicons.min.css')}}"  rel="stylesheet" >
+  
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
   <link href="{{asset('assets/css/search.css')}}" rel="stylesheet" />
   <link href="{{asset('assets/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('assets/css/all.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/css/header.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/css/home.css')}}">
+  <link href="{{asset('assets/css/all.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/css/header.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/css/home.css')}}" rel="stylesheet">
 </head>
